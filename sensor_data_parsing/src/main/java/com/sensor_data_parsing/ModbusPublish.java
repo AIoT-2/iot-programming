beb9a42b3055d8e54fed095a90cb42c998d04645
HwangSlater <doorchan23@naver.com>
2024-12-03 15:41:06 +0900

    modbus

--- sensor_data_parsing/src/main/java/com/sensor_data_parsing/ModbusPublish.java
+++ sensor_data_parsing/src/main/java/com/sensor_data_parsing/ModbusPublish.java
@@ -27,37 +27,62 @@ public class ModbusPublish {
     private static final String newMqttUsername = ""; // MQTT 사용자 이름
     private static final String newMqttPassword = ""; // MQTT 비밀번호
 
-    private static final Map<Integer, String> addressMap = new HashMap<>();
+    private static final Map<Integer, String[]> addressMap = new HashMap<>();
+
     static {
-        addressMap.put(0, "operation Heartbit");
-        addressMap.put(1, "temperature");
-        addressMap.put(2, "frequency");
-        addressMap.put(3, "program version");
-        addressMap.put(4, "present CO2 use(month)");
-        addressMap.put(6, "operation Heartbit");
-        addressMap.put(7, "temperature 1");
-        addressMap.put(8, "frequency");
-        addressMap.put(9, "program version");
-        addressMap.put(10, "present CO2 use(month)");
-        addressMap.put(12, "V123(LN) average");
-        addressMap.put(13, "V123(LL) average");
-        addressMap.put(14, "V123(LN) unbalance");
-        addressMap.put(15, "V123(LL) unbalance");
-        addressMap.put(16, "V1");
-        addressMap.put(17, "V12");
-        addressMap.put(18, "V1 unbalance");
-        addressMap.put(19, "V12 unbalance");
-        addressMap.put(20, "V2");
-        addressMap.put(21, "V23");
-        addressMap.put(22, "V2 unbalance");
-        addressMap.put(23, "V23 unbalance");
-        addressMap.put(24, "V3");
-        addressMap.put(25, "V31");
-        addressMap.put(26, "V3 unbalance");
-        addressMap.put(27, "V31 unbalance");
-        addressMap.put(28, "V1 THD");
-        addressMap.put(29, "V2 THD");
-        addressMap.put(30, "V3 THD");
+        addressMap.put(0, new String[] { "type", "1" });
+        addressMap.put(1, new String[] { "a leakage current", "1" });
+        addressMap.put(2, new String[] { "current", "100.0" });
+        addressMap.put(4, new String[] { "W", "1" });
+        addressMap.put(6, new String[] { "VAR", "1" });
+        addressMap.put(8, new String[] { "VA", "1" });
+        addressMap.put(10, new String[] { "PF average", "100.0" });
+        addressMap.put(11, new String[] { "reserved", "1" });
+        addressMap.put(12, new String[] { "current unbalance", "100.0" });
+        addressMap.put(13, new String[] { "I THD average", "100.0" });
+        addressMap.put(14, new String[] { "IGR", "10.0" });
+        addressMap.put(15, new String[] { "IGC", "10.0" });
+        addressMap.put(16, new String[] { "V1", "100.0" });
+        addressMap.put(18, new String[] { "I1", "100.0" });
+        addressMap.put(20, new String[] { "W", "1" });
+        addressMap.put(22, new String[] { "VAR", "1" });
+        addressMap.put(24, new String[] { "VA", "1" });
+        addressMap.put(26, new String[] { "volt unbalance", "100.0" });
+        addressMap.put(27, new String[] { "current unbalance", "100.0" });
+        addressMap.put(28, new String[] { "phase", "100.0" });
+        addressMap.put(29, new String[] { "power factor", "100.0" });
+        addressMap.put(30, new String[] { "I1 THD", "100.0" });
+        addressMap.put(31, new String[] { "reserved", "1" });
+        addressMap.put(32, new String[] { "V2", "100.0" });
+    }
+
+    private static final Map<Integer, String> channelMap = new HashMap<>();
+    static {
+        channelMap.put(100, "캠퍼스");
+        channelMap.put(200, "캠퍼스1");
+        channelMap.put(300, "전등1");
+        channelMap.put(400, "전등2");
+        channelMap.put(500, "전등3");
+        channelMap.put(600, "강의실b_바닥");
+        channelMap.put(700, "강의실a_바닥1");
+        channelMap.put(800, "강의실a_바닥2");
+        channelMap.put(900, "프로젝터1");
+        channelMap.put(1000, "프로젝터2");
+        channelMap.put(1100, "회의실");
+        channelMap.put(1200, "서버실");
+        channelMap.put(1300, "간판");
+        channelMap.put(1400, "페어룸");
+        channelMap.put(1500, "사무실_전열1");
+        channelMap.put(1600, "사무실_전열2");
+        channelMap.put(1700, "사무실_복사기");
+        channelMap.put(1800, "빌트인_전열");
+        channelMap.put(1900, "사무실_전열3");
+        channelMap.put(2000, "정수기");
+        channelMap.put(2100, "하이브_전열");
+        channelMap.put(2200, "바텐_전열");
+        channelMap.put(2300, "S_P");
+        channelMap.put(2400, "공조기");
+        channelMap.put(2500, "AC");
     }
 
     public static void main(String[] args) {
@@ -114,66 +139,80 @@ public class ModbusPublish {
             int quantity = 32;
 
             try {
-                // since 1.2.8
                 if (!m.isConnected()) {
                     m.connect();
                 }
 
-                ReadInputRegistersRequest request = new ReadInputRegistersRequest();
-                request.setServerAddress(slaveId);
-                request.setStartAddress(offset);
-                request.setQuantity(quantity);
-                request.setTransactionId(1);
+                while (offset <= 2400) {
+                    System.out.println(channelMap.get(offset));
 
-                ReadInputRegistersResponse response = (ReadInputRegistersResponse) m.processRequest(request);
+                    ReadInputRegistersRequest request = new ReadInputRegistersRequest();
+                    request.setServerAddress(slaveId);
+                    request.setStartAddress(offset);
+                    request.setQuantity(quantity);
+                    request.setTransactionId(1);
 
-                // 메시지를 보낼 새 MQTT 브로커로 메시지 전송
-                String newTopic = "application/modbus";
+                    ReadInputRegistersResponse response = (ReadInputRegistersResponse) m.processRequest(request);
 
-                Map<String, Object> dataMap = new HashMap<>();
+                    // 메시지를 보낼 새 MQTT 브로커로 메시지 전송
+                    String newTopic = "application/modbus";
 
-                int addressIncrement = 0;
-                // 레지스터 값을 읽고 처리
-                for (int i = 0; i < response.getHoldingRegisters().getQuantity(); i++) {
-                    int registerValue = response.getHoldingRegisters().get(i);
-                    if (i == 5 || i == 10) {
-                        addressIncrement++;
+                    Map<String, Object> dataMap = new HashMap<>();
+
+                    // 레지스터 값을 읽고 처리
+                    for (int i = 0; i < response.getHoldingRegisters().getQuantity(); i++) {
+                        int registerValue = response.getHoldingRegisters().get(i);
+                        int address = offset + i;
+
+                        if ((address - offset == 2 ||
+                                address - offset == 4 ||
+                                address - offset == 6 ||
+                                address - offset == 8 ||
+                                address - offset == 16 ||
+                                address - offset == 18 ||
+                                address - offset == 20 ||
+                                address - offset == 22 ||
+                                address - offset == 24 ||
+                                address - offset == 32) &&
+                                i < response.getHoldingRegisters().getQuantity() - 1) {
+                            registerValue = ((response.getHoldingRegisters().get(i) << 16)
+                                    | response.getHoldingRegisters().get(i + 1));
+                            i++; // 두 개의 레지스터를 처리했으므로 i를 1 증가시킴
+                        }
+
+                        String addressDescription = addressMap.get(address - offset)[0];
+                        double registerValueDouble = registerValue
+                                / Double.parseDouble(addressMap.get(address - offset)[1]);
+                        if (registerValueDouble == Math.floor(registerValueDouble)) {
+                            // registerValueDouble이 정수와 같으면 int로 변환
+                            int registerValueInt = (int) registerValueDouble;
+                            System.out.println("Address: " + address + ", Value: " + registerValueInt); // int 출력
+                            dataMap.put(addressDescription, registerValueInt); // int 값 저장
+                        } else {
+                            // 그렇지 않으면 double로 처리
+                            System.out.println("Address: " + address + ", Value: " + registerValueDouble); // double 출력
+                            dataMap.put(addressDescription, registerValueDouble); // double 값 저장
+                        }
                     }
-                    int address = offset + addressIncrement; // 주소는 i에 맞게 증가
 
-                    if ((address - offset == 12 ||
-                            address - offset == 13 ||
-                            address - offset == 16 ||
-                            address - offset == 17 ||
-                            address - offset == 20 ||
-                            address - offset == 21 ||
-                            address - offset == 24 ||
-                            address - offset == 25) &&
-                            i < response.getHoldingRegisters().getQuantity() - 1) {
-                        registerValue = (response.getHoldingRegisters().get(i) << 16)
-                                | response.getHoldingRegisters().get(i + 1);
-                        i++; // 두 개의 레지스터를 처리했으므로 i를 1 증가시킴
-                    }
+                    Map<String, Object> dataMessage = new HashMap<>();
+                    dataMessage.put("deviceName", channelMap.get(offset));
+                    dataMessage.put("data", dataMap);
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    String message = objectMapper.writeValueAsString(dataMessage);
 
-                    System.out.println("Address: " + address + ", Value: " + registerValue);
+                    // 메시지를 보낼 새 MQTT 브로커로 메시지 발행
+                    newMqttClient.toAsync().publishWith()
+                            .topic(newTopic)
+                            .payload(message.getBytes(StandardCharsets.UTF_8))
+                            .qos(MqttQos.AT_LEAST_ONCE) // QoS 1 (최소 한 번 전송)
+                            .send();
 
-                    String addressDescription = addressMap.get(address - offset);
-                    dataMap.put(addressDescription, registerValue);
-                    addressIncrement++;
+                    offset += 100;
+                    System.out.println();
+                    Thread.sleep(1000);
                 }
 
-                Map<String, Object> dataMessage = new HashMap<>();
-                dataMessage.put("deviceName", "캠퍼스");
-                dataMessage.put("data", dataMap);
-                ObjectMapper objectMapper = new ObjectMapper();
-                String message = objectMapper.writeValueAsString(dataMessage);
-
-                // 메시지를 보낼 새 MQTT 브로커로 메시지 발행
-                newMqttClient.toAsync().publishWith()
-                        .topic(newTopic)
-                        .payload(message.getBytes(StandardCharsets.UTF_8))
-                        .qos(MqttQos.AT_LEAST_ONCE) // QoS 1 (최소 한 번 전송)
-                        .send();
             } catch (ModbusProtocolException | ModbusNumberException | ModbusIOException e) {
                 e.printStackTrace();
             } finally {
