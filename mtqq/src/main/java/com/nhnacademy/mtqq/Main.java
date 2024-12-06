6eca883f91bb96906c06f495a8892a0c51b97f9e
JiwonLee-2 <this0aid@gmail.com>
2024-12-06 11:07:25 +0900

    TODO삭제

--- mtqq/src/main/java/com/nhnacademy/mtqq/Main.java
+++ mtqq/src/main/java/com/nhnacademy/mtqq/Main.java
@@ -8,12 +8,6 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 
 public class Main {
-    //TODO #0. 파라미터 체크하기    ---> OK!
-    //TODO #1. MessageData대신 map으로 데이터 받아오기!
-    //TODO #2. 디폴트 값을 설정하기! -> 현재 파일에서 config나 conf로 디폴트값을 정해줄 것.   ---> 50%
-    //TODO #3. Handler.handle() 이름 변경하거나 return하는 값을 object로 하기, 메소드와 행동이 일치하지 않음!  ---> OK?
-    //TODO #4. token값을 환경변수로 주지 말고 properties로 줄 것. ---> OK!
-
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
