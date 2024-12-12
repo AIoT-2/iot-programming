80be6bee3d5995c8cbcc9b16ffd01d8b48c14644
HwangSlater <doorchan23@naver.com>
2024-12-12 20:56:09 +0900

    d

---
+++ sensor_data_flow/src/main/java/com/sensor_data_flow/test2.java
@@ -0,0 +1,12 @@
+package com.sensor_data_flow;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class test2 {
+
+    public static void main(String[] args) {
+        log.error("asd");
+        log.info("gzdxz");
+    }
+}
